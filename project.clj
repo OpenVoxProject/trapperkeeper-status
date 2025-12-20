ba5b4f4a7c9a96045c02e4e8e2c2402b703e829e
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-12-19 17:03:42 -0800

    Updates for OpenVox

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject puppetlabs/trapperkeeper-status "1.2.1-SNAPSHOT"
+(defproject org.openvoxproject/trapperkeeper-status "1.2.1-SNAPSHOT"
   :description "A trapperkeeper service for getting the status of other trapperkeeper services."
-  :url "https://github.com/puppetlabs/trapperkeeper-status"
+  :url "https://github.com/openvoxproject/trapperkeeper-status"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
   :min-lein-version "2.9.0"
 
-  :parent-project {:coords [puppetlabs/clj-parent "7.3.7"]
+  :parent-project {:coords [org.openvoxproject/clj-parent "7.4.1-SNAPSHOT"]
                    :inherit [:managed-dependencies]}
 
   :pedantic? :abort
@@ -18,24 +18,24 @@
                  [trptcolin/versioneer]
                  [org.clojure/java.jmx]
                  [org.clojure/tools.logging]
-                 [puppetlabs/kitchensink]
-                 [puppetlabs/trapperkeeper]
-                 [puppetlabs/trapperkeeper-scheduler]
-                 [puppetlabs/ring-middleware]
-                 [puppetlabs/comidi]
-                 [puppetlabs/i18n]
-                 [puppetlabs/trapperkeeper-authorization]]
+                 [org.openvoxproject/kitchensink]
+                 [org.openvoxproject/trapperkeeper]
+                 [org.openvoxproject/trapperkeeper-scheduler]
+                 [org.openvoxproject/ring-middleware]
+                 [org.openvoxproject/comidi]
+                 [org.openvoxproject/i18n]
+                 [org.openvoxproject/trapperkeeper-authorization]]
 
-  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
-                                     :username :env/clojars_jenkins_username
-                                     :password :env/clojars_jenkins_password
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                     :username :env/CLOJARS_USERNAME
+                                     :password :env/CLOJARS_PASSWORD
                                      :sign-releases false}]]
 
   :profiles {:dev {:dependencies [[org.bouncycastle/bcpkix-jdk18on]
-                                  [puppetlabs/http-client]
-                                  [puppetlabs/trapperkeeper :classifier "test"]
-                                  [com.puppetlabs/trapperkeeper-webserver-jetty10]
-                                  [puppetlabs/kitchensink :classifier "test"]]}}
+                                  [org.openvoxproject/http-client]
+                                  [org.openvoxproject/trapperkeeper :classifier "test"]
+                                  [org.openvoxproject/trapperkeeper-webserver-jetty10]
+                                  [org.openvoxproject/kitchensink :classifier "test"]]}}
 
   :plugins [[lein-parent "0.3.9"]
-            [puppetlabs/i18n "0.9.2" :hooks false]])
+            [org.openvoxproject/i18n "0.9.2" :hooks false]])
