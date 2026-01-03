bd973f0661608613a5ddf3de271fd65f8cd2f1aa
nmburgan <13688219+nmburgan@users.noreply.github.com>
2026-01-02 20:44:30 -0800

    Fix script to regenerate test certs
    The files had moved around and this script was not updated accordingly.

--- dev-resources/gen-pki.sh
+++ dev-resources/gen-pki.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if ! [[ -d dev-resources/ssl ]]; then
-  echo "This script must be called from the root of the project and dev-resources/ssl must already exist"
+certspath="puppetlabs/trapperkeeper-status/status-proxy-service-test/ssl"
+if ! [[ -d "${certspath}" ]]; then
+  echo "This script must be called from the dev-resources directory and ${certspath} must already exist"
   exit 1
 fi
 
@@ -9,46 +10,35 @@ echo
 echo "Generating primary self-signed CA"
 openssl req -x509 \
   -newkey rsa:4096 \
-  -keyout dev-resources/ssl/ca.key \
-  -out dev-resources/ssl/ca.pem \
+  -keyout "${certspath}/ca.key" \
+  -out "${certspath}/certs/ca.pem" \
   -days 1825 -nodes \
   -extensions x509v3_CA \
-  -config dev-resources/exts.cnf \
+  -config exts.cnf \
   -subj "/C=US/ST=OR/L=Portland/O=Puppet, Inc/CN=puppet"
 
 echo
 echo "Generating node cert"
-openssl genrsa -out dev-resources/ssl/key.pem 2048
+openssl genrsa -out "${certspath}/private_keys/localhost.pem" 2048
 
 echo
 echo "Creating node CSR"
 openssl req -new -sha256 \
-  -key dev-resources/ssl/key.pem \
-  -out dev-resources/ssl/csr.pem \
+  -key "${certspath}/private_keys/localhost.pem" \
+  -out "${certspath}/csr.pem" \
   -subj "/C=US/ST=OR/L=Portland/O=Puppet, Inc/CN=localhost"
 
 echo
 echo "Signing node CSR"
 openssl x509 -req \
-  -in dev-resources/ssl/csr.pem \
-  -CA dev-resources/ssl/ca.pem \
-  -CAkey dev-resources/ssl/ca.key \
+  -in "${certspath}/csr.pem" \
+  -CA "${certspath}/certs/ca.pem" \
+  -CAkey "${certspath}/ca.key" \
   -CAcreateserial \
-  -out dev-resources/ssl/cert.pem \
+  -out "${certspath}/certs/localhost.pem" \
   -days 1825 -sha256
 
 echo
-echo "Generating alternate self-signed CA"
-openssl req -x509 \
-  -newkey rsa:4096 \
-  -keyout dev-resources/ssl/alternate-ca.key \
-  -out dev-resources/ssl/alternate-ca.pem \
-  -days 1825 -nodes \
-  -extensions x509v3_CA \
-  -config dev-resources/exts.cnf \
-  -subj "/C=US/ST=OR/L=Portland/O=Puppet, Inc/CN=alternate"
-
-
-echo
 echo "Cleaning up files that will not be used by the tests"
-rm dev-resources/ssl/{alternate-ca.key,ca.key,ca.srl,csr.pem}
+rm "${certspath}"/{ca.key,csr.pem}
+rm "${certspath}/certs/ca.srl"
